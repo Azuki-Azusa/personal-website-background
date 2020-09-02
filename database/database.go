d6c6d26b8a3a2008325690575a3284e699adf9e6
Azuki-Azusa <cai980129@vip.qq.com>
2020-09-02 17:07:45 +0800

    database

--- database/database.go
+++ database/database.go
@@ -20,7 +20,8 @@ func GetInstance() *gorm.DB {
 // Database run.
 func init() {
 	var err error
-	Db, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/personal_website?charset=utf8&parseTime=True&loc=Local")
+	// Db, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/personal_website?charset=utf8&parseTime=True&loc=Local")
+	Db, err = gorm.Open("mysql", "azuki:orangestar@tcp(database-1.cpxxkljx2bm4.ap-northeast-1.rds.amazonaws.com:3306)/personal_website?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
